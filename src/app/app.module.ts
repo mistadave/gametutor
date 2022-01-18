efd6548a91d2e37fc6f0b76f5de41eb99bef63c9
mistadave <std@lemt.ch>
2022-01-18 18:21:18 +0100

    initale routes :smile:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
