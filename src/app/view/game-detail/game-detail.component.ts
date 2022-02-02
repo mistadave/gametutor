9dca9dda6c2ab8f61b3a89bf035be115515dcb0f
mistadave <std@lemt.ch>
2022-02-02 10:26:42 +0100

    add link item, service get game

--- src/app/view/game-detail/game-detail.component.ts
+++ src/app/view/game-detail/game-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Game } from '../game';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-game-detail',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-detail.component.scss']
 })
 export class GameDetailComponent implements OnInit {
-
-  constructor() { }
+  game: Game | undefined;
+  links?: string[];
+  constructor(private gameService: GameService,
+      private activatedRoute: ActivatedRoute) {
+    
+  }
 
   ngOnInit(): void {
+    let id: string | undefined;
+    this.activatedRoute.params.subscribe(params =>{
+      id = params['id'];
+    });
+    if (id){
+      this.game = this.gameService.getById(id);
+    }
+    this.links = [];
+    for (let i = 0; i <= 10; i++) {
+      this.links?.push(`Item ${i}`);
+    }
+
+  }
+
+  private loadGame(){
+
+  }
+
+  private loadLinks() {
+
   }
 
 }
