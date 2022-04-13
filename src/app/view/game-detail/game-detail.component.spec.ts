b516101f8d4b74a4850e2d6bc8b5bdec4388217e
mistadave <std@lemt.ch>
2022-04-13 16:46:10 +0200

    fix karma tests :smile:

--- src/app/view/game-detail/game-detail.component.spec.ts
+++ src/app/view/game-detail/game-detail.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GameDetailComponent } from './game-detail.component';
 
@@ -8,7 +9,10 @@ describe('GameDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameDetailComponent ]
+      declarations: [ GameDetailComponent ],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ]
     })
     .compileComponents();
   });
