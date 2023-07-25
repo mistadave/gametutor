050f08b7d4eae5d3c9d8a3a2c5bd1b48985759b1
mistrdave <mistrdave@gmail.com>
2023-07-25 22:54:03 +0200

    add name filter

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { NameFilterPipe } from './name-filter.pipe';
 
 
 
 @NgModule({
   declarations: [
-    ButtonComponent
+    ButtonComponent,
+    NameFilterPipe
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NameFilterPipe
   ]
 })
 export class SharedModule { }
--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -7,12 +7,19 @@ import { GAMES } from '../mock-games';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  searchText: string = ''; 
   games = GAMES;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  searchChangeFn(value: string){
+    if (value.length > 2){
+      console.log('has more then 2')
+    }
+
   }
 
 }
