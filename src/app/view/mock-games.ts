34ad42a61570f6471847faf28ae26f1cb5bc3091
David Stäheli <mistrdave@gmail.com>
2023-08-05 13:41:26 +0200

    update mock

--- src/app/view/mock-games.ts
+++ src/app/view/mock-games.ts
@@ -2,7 +2,7 @@ import { Game } from './game';
 
 export const GAMES: Game[] = [
     {id: 'some-id', name: 'Call of Duty', link: 'https://www.callofduty.com/de/', genre: 'Action', platform: ['Windows 11', 'XBOX']},
-    {id: 'netsome', name: 'Fortnite', link: 'https://www.epicgames.com/fortnite/de/download', genre: 'Action', platform: ['Windows 11']},
+    {id: 'netsome', name: 'Fortnite', link: 'https://www.epicgames.com/fortnite/', genre: 'Action', platform: ['Windows 11']},
     {id: 'fads', name: 'Counter Strike Source', link: 'https://blog.counter-strike.net/', genre: 'Action'},
-    {id: 'as32', name: 'World of Warcraft', link: 'https://wow.com', genre: 'MultiPlayer'}
+    {id: 'as32', name: 'World of Warcraft', link: 'https://wow.com', genre: 'MultiPlayer', platform: ['PC']}
 ]
